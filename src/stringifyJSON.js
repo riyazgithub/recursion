40fd183b721a82c74a1c82341209698b840417f6
riyazgithub <sunez.riyaz@gmail.com>
2016-08-31 15:31:38 +0530

    Implemented stringifyJSON
    Part 1 done.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,48 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function(str) {
   // your code goes here
+  	switch (typeof str)
+	{
+	case 'number':
+	case 'boolean':
+	return str.toString();
+	break;
+	case 'undefined':
+	case 'function':
+	return null;
+	break;
+	case 'string':
+	return '"' + str + '"';
+	break;
+	case 'object':
+	var retArr = []
+	if(Array.isArray(str)){
+	
+	for(var i=0;i< str.length; i++){
+		var val = stringifyJSON(str[i]);
+		if(val !== null) {
+			retArr.push(val)
+
+		}
+	}
+	return '[' + retArr.join(',') + ']';
+	}
+	else if (str == null){
+		return 'null'
+	}
+	else{
+		Object.keys(str).forEach(function(x){
+			var value = stringifyJSON(str[x]);
+			if (value !== null) {
+				retArr.push('"' + x + '"' + ':' + stringifyJSON(str[x]));
+			}
+	});
+	return '{' + retArr.join(',') + '}';
+	}
+	}
+
 };
+
+debug(stringifyJSON([1, 'false', false]))
